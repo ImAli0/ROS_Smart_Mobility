d4784af2d66a4dddf62b8df337b0eba989dd3544
Ali <113502495+ImAli0@users.noreply.github.com>
2023-09-29 22:21:27 +0900

    Update SM_week4_turtlesim, rqt.sh

--- Week4/SM_week4_turtlesim, rqt.sh
+++ Week4/SM_week4_turtlesim, rqt.sh
@@ -14,3 +14,32 @@ sudo apt install ros-humble-turtlesim
 echo " Installing rqt "
 sudo apt install ~nros-humble-rqt*
 
+ros2 run turtlesim turtle_teleop_key --ros-args --remap turtle1/cmd_vel:=turtle2/cmd_vel
+
+ros2 node list
+
+ros2 run turtlesim turtlesim_node --ros-args --remap __node:=my_turtle
+
+ros2 node info /my_turtle
+
+rqt_graph
+
+ros2 topic echo /turtle1/cmd_vel
+
+ros2 topic info /turtle1/cmd_vel
+
+ros2 interface show geometry_msgs/msg/Twist
+
+ros2 topic pub --once /turtle1/cmd_vel geometry_msgs/msg/Twist "{linear: {x: 2.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 1.8}}"
+
+ros2 topic hz /turtle1/pose
+
+ros2 service type /clear
+
+ros2 service list -t
+
+ros2 service find std_srvs/srv/Empty
+
+ros2 interface show turtlesim/srv/Spawn
+
+ros2 service call /spawn turtlesim/srv/Spawn "{x: 2, y: 2, theta: 0.2, name: ''}"
